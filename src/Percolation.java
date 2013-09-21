f8faf5644212020dae7d15454f9c9f66f9570553
Caleb Everett <everett.caleb@gmail.com>
2013-09-21 15:22:24 -0400

    Add percolation skeleton class

---
+++ src/Percolation.java
@@ -0,0 +1,32 @@
+public class Percolation {
+
+  private QuickUnionUf matrix;
+
+  // create N-by-N grid, with all sites blocked
+  public Percolation(int N) {
+    matrix = new QuickUnionUf(N);
+  }
+
+  // open site (row i, col j) if it is not already
+  public void open(int i, int j) {
+  }
+
+  // is site (row i, col j) open?
+  public boolean isOpen(int i, int j) {
+    return false;
+  }
+
+  // is site (row i, col j) full?
+  public boolean isFull(int i, int j) {
+    return false;
+  }
+
+  // does the system percolate?
+  public boolean percolates() {
+    return false;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("hello");
+  }
+}
