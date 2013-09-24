9881085da211d114d686e44316300a8cd6608ef5
Caleb Everett <everett.caleb@gmail.com>
2013-09-24 15:09:22 -0400

    Improve visualizer preformance

--- src/PercolationVisualizer.java
+++ src/PercolationVisualizer.java
@@ -13,70 +13,79 @@ import java.util.NoSuchElementException;
 
 public class PercolationVisualizer {
 
-  private Draw draw;
+  private final Draw draw;
 
-  public PercolationVisualizer() {
+  private final Percolation perc;
+
+  // Record the last color painted to a cell.
+  private final Color[][] matrix;
+
+  public PercolationVisualizer(Percolation perc) {
+    this.perc = perc;
+
+    matrix = new Color[perc.size()][perc.size()];
+
     draw = new Draw();
+
     // enable difffered drawing.
     draw.show(0);
   }
 
-  public void draw(Percolation perc) {
-    draw(perc, 0);
-  }
-  public void draw(Percolation perc, int wait) {
-    int n = perc.size();
+  // Draw all cells.
+  public void draw() { draw(0); }
+  public void draw(int wait) {
+    final int n = perc.size();
 
-    draw.clear();
     draw.setXscale(0, (double) n + 2);
     draw.setYscale(0, (double) n + 2);
 
-
     for (int j = 0; j < n; j++) {
       for (int i = 0; i < n; i++) {
+        final Color last_color = matrix[i][j];
+        final Color color;
         if (perc.is_full(i,j)) {
-          draw_full(i, j);
+          color = Draw.BOOK_BLUE;
         }
         else if (perc.is_open(i,j)) {
-          draw_open(i, j);
+          color = Draw.WHITE;
         }
         else {
-          draw_closed(i, j);
+          color = Draw.BLACK;
         }
+
+
+        if (color != last_color) {
+          draw_cell(i, j, color);
+        }
+        matrix[i][j] = color;
       }
     }
     draw.show(wait);
-
-    //wait_for_keypress();
   }
 
+  // draw a cell
   private void draw_cell(int i, int j, Color color) {
+    // canvas places 0,0 in bottom left, we want it in top left.
+    final double j_draw = ((double) perc.size() - j) + 1.5;
+    final double i_draw = ((double) i) + 1.5;
+
     draw.setPenColor(color);
-    draw.filledSquare(((double) i) + 1.5, ((double) j) + 1.5, 0.5);
+    draw.filledSquare(i_draw, j_draw, 0.5);
     draw.setPenColor(Draw.BLACK);
-    draw.square(((double) i) + 1.5, ((double) j) + 1.5, 0.5);
-  }
-
-  private void draw_full(int i, int j) {
-    draw_cell(i, j, Draw.BOOK_BLUE);
-  }
-
-  private void draw_open(int i, int j) {
-    draw_cell(i, j, Draw.WHITE);
-  }
-
-  private void draw_closed(int i, int j) {
-    draw_cell(i, j, Draw.BLACK);
+    draw.square(i_draw, j_draw, 0.5);
   }
 
   public static void main(String[] args) {
 
     int n = StdIn.readInt();
 
-    PercolationVisualizer viz = new PercolationVisualizer();
     Percolation perc = new Percolation(n);
+    PercolationVisualizer viz = new PercolationVisualizer(perc);
 
-    viz.draw(perc);
+    viz.draw();
+
+    double draw_time = 0;
+    double open_time = 0;
 
     try {
       while (!StdIn.isEmpty()) {
@@ -84,16 +93,23 @@ public class PercolationVisualizer {
         int i = StdIn.readInt();
         int j = StdIn.readInt();
 
+        Stopwatch open_sw = new Stopwatch();
         // open the cell
         perc.open(i,j);
+        open_time += open_sw.elapsedTime();
 
 
+        Stopwatch draw_sw = new Stopwatch();
         // draw the percolation matrix
-        viz.draw(perc, 50);
+        viz.draw();
+        draw_time += draw_sw.elapsedTime();
       }
     } catch (NoSuchElementException ex) {
       System.err.println("Unexpectantly Reached end of input while proccessing a cell");
     }
+
+    System.out.printf("open: %f%n", open_time);
+    System.out.printf("draw: %f%n", draw_time);
 
     System.exit(0);
 
